b1ffc735d3d17ba1cb03173f2047478bf35d5f7f
surbhimiglani <me.surbhimiglani@gmail.com>
2020-05-24 19:13:02 -0400

    opo

---
+++ build/precache-manifest.ee635147550587cfc8ec670d1858d6ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83266000ce76fdaf5974c58749c892f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9733ce2fe6df47522607",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "62979ed79edb5cc32cfe",
+    "url": "/static/css/main.410a30eb.chunk.css"
+  },
+  {
+    "revision": "9733ce2fe6df47522607",
+    "url": "/static/js/2.b267a338.chunk.js"
+  },
+  {
+    "revision": "bd32164b0b1f07ae79545cc1dff28ec5",
+    "url": "/static/js/2.b267a338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62979ed79edb5cc32cfe",
+    "url": "/static/js/main.480f7d63.chunk.js"
+  },
+  {
+    "revision": "8e5980586e4d36adc670",
+    "url": "/static/js/runtime-main.f7e11ae5.js"
+  }
+]);
